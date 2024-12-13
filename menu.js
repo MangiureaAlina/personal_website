0c4c8e86ec062d45ada35a1615e6ffc05c240dd0
MangiureaAlina <alinadanielamangiurea@gmail.com>
2024-12-13 13:22:01 +0200

    tema2TW

---
+++ menu.js
@@ -0,0 +1,18 @@
+ 
+     
+        function toggleMenu() {
+            const navLinks = document.getElementById('navLinks');
+            navLinks.classList.toggle('active'); // Adaugă/Elimină clasa "active"
+        }
+   
+        
+        function toggleMenu() {
+          var menu = document.getElementById("myLinks");
+          if (menu.style.display === "block") {
+            menu.style.display = "none";
+          } else {
+            menu.style.display = "block";
+          }
+        }
+        
+    
